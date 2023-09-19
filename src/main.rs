39d0c8a2c838da2611ec77dab0d40e6c25fffa4d
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-09-19 19:41:08 +0200

    it's bad but it works

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let (a, b) = longdiv(args[1].parse::<i32>().unwrap(), args[2].parse::<i32>().unwrap());
+
+    println!("{} R {}", a, b);
+}
+
+fn longdiv(a: i32, b: i32) -> (i32, i32) {
+    return ((b/a) as i32, b % a);
+}
+
+
