420337816ada015a90630f493694af5b262f67b3
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-09-20 18:03:43 +0200

    new error + f64 ipv i32

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,21 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (a, b) = longdiv(args[1].parse::<i32>().unwrap(), args[2].parse::<i32>().unwrap());
+    if args.len() != 3 {
+        println!("please input 2 numbers");
+        return;
+    }
+
+    let (a, b) = longdiv(
+        args[1].parse::<f64>().expect("please input 2 numbers"),
+        args[2].parse::<f64>().expect("please input 2 numbers")
+    );
 
     println!("{} R {}", a, b);
 }
 
-fn longdiv(a: i32, b: i32) -> (i32, i32) {
-    return ((a/b) as i32, a % b);
+fn longdiv(a: f64, b: f64) -> (i32, i32) {
+    return ((a/b) as i32, (a%b) as i32);
 }
 
 
