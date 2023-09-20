1f4b49a143e9c72409c89790cc577357e50e7d0d
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-09-20 18:23:22 +0200

    i dunno

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 use std::env;
 
-fn main() {
+fn main()
+{
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 {
+    if args.len() != 3
+    {
         println!("please input 2 numbers");
         return;
     }
+    
+    if args[1].parse::<f64>().is_err() ||
+       args[2].parse::<f64>().is_err()
+    {
+        println!("inputs are not numbers");
+        return;
+    }
 
-    let (a, b) = longdiv(
-        args[1].parse::<f64>().expect("please input 2 numbers"),
-        args[2].parse::<f64>().expect("please input 2 numbers")
+    let (a, b) = longdiv
+    (
+        args[1].parse::<f64>().unwrap(),
+        args[2].parse::<f64>().unwrap()
     );
 
     println!("{} R {}", a, b);
 }
 
-fn longdiv(a: f64, b: f64) -> (i32, i32) {
+fn longdiv(a: f64, b: f64) -> (i32, i32)
+{
     return ((a/b) as i32, (a%b) as i32);
 }
 
